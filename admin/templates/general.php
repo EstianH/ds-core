5ad64b6d0d787df1ba885ece3c833ae1915ef327
Estian Hough <support@divspot.co.za>
2019-07-08 10:42:19 +0200

    DS Core assets update.

--- admin/templates/general.php
+++ admin/templates/general.php
@@ -1,33 +1,36 @@
 <?php if(!defined('ABSPATH')) exit; ?>
 
 <div class="ds-wrapper">
-	<h1><?php _e('Thank you for using divSpot'); ?></h1>
+	<h1 class="ds-pb-2"><?php _e('Thank you for using divSpot'); ?></h1>
 	<div class="wrap ds-mt-0">
 		<h2 class="ds-pt-0 ds-pb-0"></h2><!-- WP Notices render after the first <h2> tag in class="wrap" -->
-		<div class="ds-blocks-container">
-			<div class="ds-row clearfix ds-row-equal-height">
-				<div class="ds-block ds-col ds-col-6 ds-mt-2">
-					<label class="ds-block-title ds-pt-2 ds-pr-2 ds-pb-2 ds-pl-2">
-						<h2 class="ds-mt-0 ds-mb-0">
-							<span class="dashicons dashicons-feedback"></span>
-							<?php _e('Support'); ?>
-						</h2>
-					</label>
-					<div class="ds-pt-2 ds-pr-2 ds-pb-2 ds-pl-2">
-						<p>If you require assistance please submit a support request ticket on divSpot by filling in the <a href="<?php echo DIVSPOT_URL; ?>/support" target="_blank">support form</a>.</p>
-					</div>
+		<div class="ds-row ds-row-full ds-row-md-halves ds-row-lg-thirds">
+			<div class="ds-block">
+				<label class="ds-block-title">
+					<h2 class="ds-mt-0 ds-mb-0">
+						<span class="dashicons dashicons-feedback"></span>
+						<?php _e( 'Support', 'ds-core' ); ?>
+					</h2>
+				</label>
+				<div class="ds-block-body">
+					<?php _e(
+						'If you require assistance please submit a support request on the divSpot website by filling in the <a href="' . DIVSPOT_URL . '/support" target="_blank">support form</a>.',
+						'ds-core'
+					); ?>
 				</div>
-				<div class="ds-block ds-col ds-col-6 ds-mt-2">
-					<label class="ds-block-title ds-pt-2 ds-pr-2 ds-pb-2 ds-pl-2">
-						<h2 class="ds-mt-0 ds-mb-0">
-							<span class="dashicons dashicons-feedback"></span>
-							<?php _e('Review'); ?>
-						</h2>
-					</label>
-					<div class="ds-pt-2 ds-pr-2 ds-pb-2 ds-pl-2">
-						Thank you for supporting divSpot. If you like like our plugins please support us by testing some
-						of our other products at <br /><a href="<?php echo DIVSPOT_URL; ?>" target="_blank"><?php echo DIVSPOT_URL; ?></a>.
-					</div>
+			</div>
+			<div class="ds-block">
+				<label class="ds-block-title">
+					<h2 class="ds-mt-0 ds-mb-0">
+						<span class="dashicons dashicons-feedback"></span>
+						<?php _e( 'Review', 'ds-core' ); ?>
+					</h2>
+				</label>
+				<div class="ds-block-body">
+					<?php _e(
+						'Thank you for supporting divSpot. If you like our plugins please support us by testing some of our other products on the <a href="' . DIVSPOT_URL . '" target="_blank">divSpot website</a>.',
+						'ds-core'
+					); ?>
 				</div>
 			</div>
 		</div>
